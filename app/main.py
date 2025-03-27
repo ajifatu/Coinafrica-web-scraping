198bea7f3638bc079bb415f26ba851b00a40a62a
ajifatu <afmim221@gmail.com>
2025-03-27 02:40:20 +0000

    Adding project first requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-raw_data_webscraper =pd.read_csv("data/coinafrique_rawData_webScraper.csv")
-st.write(raw_data_webscraper)
+# raw_data_webscraper =pd.read_csv("data/coinafrique_rawData_webScraper.csv")
+# st.write(raw_data_webscraper)
+
+
+st.title("CoinAfrique Animal Scraper")
+st.write("Bienvenue sur votre app de collecte et visualisation de données !")
